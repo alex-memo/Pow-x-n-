c6baf235ed18df05f495a40c74611c010c18a50d
memo <alexis45645@gmail.com>
2022-11-05 00:19:04 -0400

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,11 @@
+
+public class Solution {//q50
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(myPow(2,10));
+	}
+    public static double myPow(double x, int n) {
+        return Math.pow(x, n);
+    }
+}
